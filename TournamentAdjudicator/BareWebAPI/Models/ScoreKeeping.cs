192308cf99baf90ca8c19a9c6834b71ff3528c04
bdavs <rdavis2@umassd.edu>
2016-10-11 14:10:39 -0400

    merging scorekeeping stuff. scorekeeping is going in the main folder now

--- TournamentAdjudicator/BareWebAPI/Models/ScoreKeeping.cs
+++ TournamentAdjudicator/BareWebAPI/Models/ScoreKeeping.cs
@@ -4,9 +4,61 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TournamentAdjudicator.Models
+namespace TournamentAdjudicator
 {
     class ScoreKeeping
     {
+        // Vidhya's algorithm for calculating scores
+        // Still needs to be fully tested
+        public int CalculateScore(int letterCount, bool OneTileHigh, bool Letters7, bool QuOneTile)
+        {
+            int result;
+	        int Score; 
+            
+            if(OneTileHigh && QuOneTile)
+   	        {
+     	        result=(letterCount*2)+2;
+			    if(Letters7)
+				{
+				    Score=result+20;
+                }
+                else
+				{
+				    Score=result;
+                }
+
+                return Score;
+   	        }	
+                 
+            else if(OneTileHigh)
+   	        {
+     	        result=letterCount*2;
+			    if(Letters7)
+				{
+					Score=result+20;
+                }
+                else
+			    {
+			        Score=result;
+                }
+
+                return Score;
+   	        }	
+	
+            else
+   	        {
+                result=letterCount;
+		        if(Letters7)
+		        {
+	    	        Score=result+20;
+                }
+                else
+	            {
+		            Score=result;
+                }
+
+                return Score;
+   	        }
+        }
     }
 }
