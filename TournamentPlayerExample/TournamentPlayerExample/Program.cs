192916a5133514551750f83ad5fb70bae09491dd
bdavs <rdavis2@umassd.edu>
2016-10-30 18:54:31 -0400

    TournamentPlayerExample can now join a game and request the board state

--- TournamentPlayerExample/TournamentPlayerExample/Program.cs
+++ TournamentPlayerExample/TournamentPlayerExample/Program.cs
@@ -3,50 +3,70 @@ using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Web;
-using System.Threading.Tasks;
+using System.Threading.Tasks; 
+using Newtonsoft.Json;
 
 namespace TournamentPlayerExample
 {
     public class Payload
     {
-        public int Id { get; set; }
+        public int ID { get; set; }
         public string Hash { get; set; }
         public string[] Letters { get; set; }
         public string[,,] Board { get; set; }
     }
-
-    class Program
+    public static class GameNetworkCommuncation
     {
         static HttpClient client = new HttpClient();
+        static Payload myPayload = new Payload();
 
-      /*  static void ShowProduct(Product product)
+        /*  static void ShowProduct(Product product)
+          {
+              Console.WriteLine($"Name: {product.Name}\tPrice: {product.Price}\tCategory: {product.Category}");
+          }
+          */
+        static async Task<Uri> JoinGame()
         {
-            Console.WriteLine($"Name: {product.Name}\tPrice: {product.Price}\tCategory: {product.Category}");
-        }
-        */
-        static async Task<Uri> CreatePayloadAsync(Payload payload)
-        {
-            HttpResponseMessage response = await client.PostAsJsonAsync("api/users", payload);
+            //sends a get request to http://localhost:62027/api/user to join game
+            HttpResponseMessage response = await client.GetAsync(client.BaseAddress.AbsoluteUri + "api/user");
+
+            //makes sure the action was performed correctly
             response.EnsureSuccessStatusCode();
 
-            // return URI of the created resource.
-            return response.Headers.Location;
-        }
-
-        static async Task<Payload> GetPayloadAsync(string path)
-        {
-            Payload payload = null;
-            HttpResponseMessage response = await client.GetAsync(path);
+            //if it was performed correctly, write the response into the data structure.
             if (response.IsSuccessStatusCode)
             {
-                payload = await response.Content.ReadAsAsync<Payload>();
+                myPayload = await response.Content.ReadAsAsync<Payload>();
             }
-            return payload;
+
+            //return location
+            return (response.Headers.Location);
         }
 
-        static async Task<Payload> UpdateProductAsync(Payload payload)
+        static async Task<Payload> GetGamestate()
         {
-            HttpResponseMessage response = await client.PutAsJsonAsync($"api/game/{payload.Id}", payload);
+
+            var request = new HttpRequestMessage()
+            {
+                RequestUri = new Uri(client.BaseAddress.AbsoluteUri+$"api/game/{myPayload.ID}"),
+                Method = HttpMethod.Get,
+            };
+            //request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("Hash"));
+            client.DefaultRequestHeaders.Add("Hash", myPayload.Hash);
+            //client.DefaultRequestHeaders["Hash"] = myPayload.Hash;
+            //client.DefaultRequestHeaders.Add("Hash", myPayload.Hash);
+
+            HttpResponseMessage response = await client.SendAsync(request);
+            if (response.IsSuccessStatusCode)
+            {
+                myPayload = await response.Content.ReadAsAsync<Payload>();
+            }
+            return myPayload;
+        }
+
+        static async Task<Payload> UpdatePayloadAsync(Payload payload)
+        {
+            HttpResponseMessage response = await client.PutAsJsonAsync($"api/game/{payload.ID}", payload);
             response.EnsureSuccessStatusCode();
 
             // Deserialize the updated product from the response body.
@@ -59,13 +79,7 @@ namespace TournamentPlayerExample
             HttpResponseMessage response = await client.DeleteAsync($"api/products/{id}");
             return response.StatusCode;
         }
-
-        static void Main()
-        {
-            RunAsync().Wait();
-        }
-
-        static async Task RunAsync()
+        public static async Task RunAsync()
         {
             client.BaseAddress = new Uri("http://localhost:62027/");
             client.DefaultRequestHeaders.Accept.Clear();
@@ -73,28 +87,30 @@ namespace TournamentPlayerExample
 
             try
             {
-                // Create a new product
-                Payload payload = new Payload();
+                //join the game and get ID and Hash
+                await JoinGame();
+                Console.WriteLine("I am user number {0} with hash code {1}", myPayload.ID, myPayload.Hash);
+                await GetGamestate();
+                Console.WriteLine("I got the board state and letters");
 
-                var url = await CreatePayloadAsync(payload);
-                Console.WriteLine($"Created at {url}");
+
 
                 // Get the product
-                payload = await GetPayloadAsync(url.PathAndQuery);
+                //payload = await GetPayloadAsync(url.PathAndQuery);
                 //ShowProduct(product);
 
                 // Update the product
-               // Console.WriteLine("Updating price...");
+                // Console.WriteLine("Updating price...");
                 //payload.Price = 80;
-                await UpdateProductAsync(payload);
+                //await UpdatePayloadAsync(payload);
 
                 // Get the updated product
-                payload = await GetPayloadAsync(url.PathAndQuery);
-               // ShowProduct(payload);
+                //payload = await GetPayloadAsync(url.PathAndQuery);
+                // ShowProduct(payload);
 
                 // Delete the product
                 //var statusCode = await DeleteProductAsync(payload.Id);
-               // Console.WriteLine($"Deleted (HTTP Status = {(int)statusCode})");
+                // Console.WriteLine($"Deleted (HTTP Status = {(int)statusCode})");
 
             }
             catch (Exception e)
@@ -104,6 +120,17 @@ namespace TournamentPlayerExample
 
             Console.ReadLine();
         }
+    }
+    class Program
+    {
+
+        
+        static void Main()
+        {
+            GameNetworkCommuncation.RunAsync().Wait();
+        }
+
+       
 
     }
 }
