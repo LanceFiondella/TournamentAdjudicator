6047e5d1585dfa36ba516907ec9a88a912737a85
bdavs <rdavis2@umassd.edu>
2016-10-30 16:27:09 -0400

    added skeleton code for example

---
+++ TournamentPlayerExample/TournamentPlayerExample/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace TournamentPlayerExample
+{
+    class Program
+    {
+        static HttpClient client = new HttpClient();
+
+        static void Main()
+        {
+            RunAsync().Wait();
+        }
+
+        static async Task RunAsync()
+        {
+            Console.ReadLine();
+        }
+    }
+}
