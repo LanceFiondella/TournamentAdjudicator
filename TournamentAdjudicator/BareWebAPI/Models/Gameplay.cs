e5720f60815894969458a6647ef06c764bac1422
bdavs <rdavis2@umassd.edu>
2016-12-12 12:49:31 -0500

    will not try to distribute letters if there are none in the bag

--- TournamentAdjudicator/BareWebAPI/Models/Gameplay.cs
+++ TournamentAdjudicator/BareWebAPI/Models/Gameplay.cs
@@ -354,30 +354,14 @@ namespace TournamentAdjudicator.Models
 
         }
 
-        /*// for debug only
-        static string[] desiredLetters = 
-        {"P","E","B","I","N","U","B","O","L","E","W","J","K","D","F","U","R","S","A","K","D","A"};
-        //{"P","E","E","R","L","E","B","B","A"};
-        //1   2   3   4   5   6   7   1   2   3   4   5   6   7   1   2   3   4   5   6   7   1   2   3   4   5   6   7
-        static int desiredMarker = 0;
-        static int desiredLettersSize = desiredLetters.Length;
         public static void give_letters(Player p, int needed)
         {
             Random rnd = new Random();
             int start2;
-
-            for (int i = 0; i < needed; i++)
+            if (bag.Count > 0)
             {
-                if (desiredMarker < desiredLettersSize)
-                {
-                    if (bag.Contains(desiredLetters[desiredMarker]))
-                    {
-                        p.addSingleLetter(desiredLetters[desiredMarker]);
-                        bag.Remove(desiredLetters[desiredMarker]);
-                        desiredMarker++;
-                    }
-                }
-                else
+
+                for (int i = 0; i < needed; i++)
                 {
                     start2 = rnd.Next(0, bag.Count - 1);
                     p.addSingleLetter(bag[start2]);
@@ -385,23 +369,7 @@ namespace TournamentAdjudicator.Models
                     bag.Remove(bag[start2]);
                 }
             }
-        }*/
-
-
-        // for actual gameplay!!!!
-        public static void give_letters(Player p, int needed)
-        {
-            Random rnd = new Random();
-            int start2;
-
-            for (int i = 0; i < needed; i++)
-            {
-
-                start2 = rnd.Next(0, bag.Count - 1);
-                p.addSingleLetter(bag[start2]);
-                //Console.WriteLine("p: " + p.Letters[i]);
-                bag.Remove(bag[start2]);
-            }
+            
         }
 
 
